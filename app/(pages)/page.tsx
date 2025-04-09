d1338697fd26cfbdd33195178fb5d4febae8f363
Raul Sigoli <raul_sigoli@hotmail.com>
2025-04-09 10:59:09 -0300

    feat: Página Criar Link
    Nesta aula, vamos estilizar a página de criação de links. O foco é validar o input do usuário, tratando letras maiúsculas e checando se o link já existe. Usamos o Next.js para organizar as páginas, criando uma nova pasta para manter tudo conciso. A página terá um header, um formulário para o usuário digitar o link e um botão para criar. Também implementaremos uma mensagem de erro caso algo dê errado. As funcionalidades completas serão adicionadas posteriormente.

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,18 @@
+import FAQ from "../components/commons/landing-page/faq";
+import Pricing from "../components/commons/landing-page/pricing";
+import VideoExplanation from "../components/commons/landing-page/video-explanation";
+import Header from "../components/landing-page/header";
+import Hero from "../components/landing-page/hero";
+
+export default function Home() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Header />
+      <Hero />
+
+      <VideoExplanation />
+      <Pricing />
+      <FAQ />
+    </div>
+  );
+}
